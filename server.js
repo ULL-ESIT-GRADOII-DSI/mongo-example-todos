3b8b2c0ae604fa7489c0d196374c25ba2d8a0d09
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-04-13 22:27:52 +0100

    updating

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require("express"),
-    http = require("http"),
-    // import the mongoose library
     mongoose = require("mongoose"),
     app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/client"));
-app.use(express.bodyParser());
+
+//app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // connect to the amazeriffic data store in mongo
 mongoose.connect('mongodb://localhost/amazeriffic');
@@ -18,11 +21,10 @@ var ToDoSchema = mongoose.Schema({
 
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 
-http.createServer(app).listen(3000);
 
 app.get("/todos.json", function (req, res) {
     ToDo.find({}, function (err, toDos) {
-	res.json(toDos);
+  res.json(toDos);
     });
 });
 
@@ -30,21 +32,25 @@ app.post("/todos", function (req, res) {
     console.log(req.body);
     var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
     newToDo.save(function (err, result) {
-	if (err !== null) {
-	    // the element did not get saved!
-	    console.log(err);
-	    res.send("ERROR");
-	} else {
-	    // our client expects *all* of the todo items to be returned, so we'll do
-	    // an additional request to maintain compatibility
-	    ToDo.find({}, function (err, result) {
-		if (err !== null) {
-		    // the element did not get saved!
-		    res.send("ERROR");
-		}
-		res.json(result);
-	    });
-	}
+  if (err !== null) {
+      // the element did not get saved!
+      console.log(err);
+      res.send("ERROR");
+  } else {
+      // our client expects *all* of the todo items to be returned, so we'll do
+      // an additional request to maintain compatibility
+      ToDo.find({}, function (err, result) {
+    if (err !== null) {
+        // the element did not get saved!
+        res.send("ERROR");
+    }
+    res.json(result);
+      });
+  }
     });
 });
 
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
